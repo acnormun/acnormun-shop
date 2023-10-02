98f0c9da6ec977bd006230b2fd416f9d823ad87f
Ana Clara <anaclaranoronha.m@gmail.com>
2023-10-02 12:38:56 -0300

    Add next image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import { globalStyles } from '@/styles/globals';
 import type { AppProps } from 'next/app'
 import bee from '@/assets/bee.svg'
+import { Container, Header } from '@/styles/pages/app';
+import Image from 'next/image';
 
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
   return(
-    <div>
-      <header>
-        <img src={bee.src} alt="" height={70} />
-      </header>
+    <Container>
+      <Header>
+        <Image src={bee} alt="" height={70}/>
+      </Header>
        <Component {...pageProps} />
 
-    </div>
+    </Container>
     )
 }
